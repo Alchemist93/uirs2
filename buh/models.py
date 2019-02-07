1e278d0279d7c46e70b26a29368c359114c45248
Alchemist93 <40356583+Alchemist93@users.noreply.github.com>
2019-02-07 10:41:12 +0500

    added interaction model

--- buh/models.py
+++ buh/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -9,10 +10,29 @@ class Counterparty(models.Model):
     inn = models.IntegerField(max_length=20, verbose_name='ИНН')
     rchet = models.IntegerField(max_length=50, verbose_name='Рассчетный счет')
     oked = models.IntegerField(max_length=20, verbose_name='ОКЭД')
+    slug = models.SlugField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('counterparty_detail', kwargs={'counterparty_slug': self.slug})
+
+    class Meta:
+        verbose_name = 'Контрагент'
+        verbose_name_plural = 'Контрагенты'
+
+
+class Interactions(models.Model):
+    name = models.ForeignKey(Counterparty, on_delete=models.CASCADE,verbose_name='Наименование')
+    count = models.DecimalField(max_digits=20, verbose_name='Сумма')
+    comment = models.IntegerField(max_length=600, verbose_name='Комментарий')
+    scan = models.ImageField(blank=True, verbose_name='Платежка или основание')
+    slug = models.SlugField(blank=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = 'Контрагент'
-        verbose_name_plural = 'Контрагенты'
+        verbose_name = 'Операция'
+        verbose_name_plural = 'Операции'
