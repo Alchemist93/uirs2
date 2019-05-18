1c005603881a6fe5d97c8f15e612a74da5ad3b5d
Alchemist93 <40356583+Alchemist93@users.noreply.github.com>
2019-05-18 08:31:39 +0500

    Lab Tests updated

--- laboratory/models.py
+++ laboratory/models.py
@@ -56,51 +56,132 @@ class Bonderit(models.Model):
         verbose_name_plural = 'Bonderit M-CR-NR'
 
 
-class LabTests(models.Model):
+class DryLayer(models.Model):
+    dry_layer_thickness_1 = models.IntegerField(verbose_name='Толщина сухого слоя мкм 1 замер')
+    dry_layer_thickness_2 = models.IntegerField(verbose_name='Толщина сухого слоя мкм 2 замер')
+    dry_layer_thickness_3 = models.IntegerField(verbose_name='Толщина сухого слоя мкм 3 замер')
+    dry_layer_thickness_sr = models.IntegerField(verbose_name='Толщина сухого слоя мкм среднее')
+
+    class Meta:
+        verbose_name = 'Толщина сухого слоя'
+        verbose_name_plural = 'Толщина сухого слоя'
+        abstract = True
+
+
+class PrimerLayer(models.Model):
+    primer_layer_thickness_1 = models.IntegerField(verbose_name='Толщина слоя грунта 1 замер')
+    primer_layer_thickness_2 = models.IntegerField(verbose_name='Толщина слоя грунта 2 замер')
+    primer_layer_thickness_3 = models.IntegerField(verbose_name='Толщина слоя грунта 3 замер')
+    primer_layer_thickness_sr = models.IntegerField(verbose_name='Толщина слоя грунта среднее')
+
+    class Meta:
+        verbose_name = 'Толщина слоя грунта'
+        verbose_name_plural = 'Толщина слоя грунта'
+        abstract = True
+
+
+class ReverseLayer(models.Model):
+    reverse_layer_thickness_1 = models.IntegerField(verbose_name='Толщина обратного слоя мкм 1 замер')
+    reverse_layer_thickness_2 = models.IntegerField(verbose_name='Толщина обратного слоя мкм 2 замер')
+    reverse_layer_thickness_3 = models.IntegerField(verbose_name='Толщина обратного слоя мкм 3 замер')
+    reverse_layer_thickness_sr = models.IntegerField(verbose_name='Толщина обратного слоя мкм среднее')
+
+    class Meta:
+        verbose_name = 'Толщина обратного слоя'
+        verbose_name_plural = 'Толщина обратного слоя'
+        abstract = True
+
+
+class Brilliance(models.Model):
+    brilliance_1 = models.IntegerField(verbose_name='Блеск % 1 замер')
+    brilliance_2 = models.IntegerField(verbose_name='Блеск % 2 замер')
+    brilliance_3 = models.IntegerField(verbose_name='Блеск % 3 замер')
+    brilliance_sr = models.IntegerField(verbose_name='Блеск % среднее')
+
+    class Meta:
+        verbose_name = 'Блеск %'
+        verbose_name_plural = 'Блеск %'
+        abstract = True
+
+
+class NotchedAdhesion(models.Model):
+    notched_adhesion_1 = models.IntegerField(verbose_name='Адгезия с надрезом 1 замер')
+    notched_adhesion_2 = models.IntegerField(verbose_name='Адгезия с надрезом 2 замер')
+    notched_adhesion_3 = models.IntegerField(verbose_name='Адгезия с надрезом 3 замер')
+    notched_adhesion_sr = models.IntegerField(verbose_name='Адгезия с надрезом среднее')
+
+    class Meta:
+        verbose_name = 'Адгезия с надрезом'
+        verbose_name_plural = 'Адгезия с надрезом'
+        abstract = True
+
+
+class ResistanceToShock(models.Model):
+    resistance_to_shock_1 = models.IntegerField(verbose_name='Стойкость к обратному удару 1 замер')
+    resistance_to_shock_2 = models.IntegerField(verbose_name='Стойкость к обратному удару 2 замер')
+    resistance_to_shock_3 = models.IntegerField(verbose_name='Стойкость к обратному удару 3 замер')
+    resistance_to_shock_sr = models.IntegerField(verbose_name='Стойкость к обратному удару среднее')
+
+    class Meta:
+        verbose_name = 'Стойкость к обратному удару'
+        verbose_name_plural = 'Стойкость к обратному удару'
+        abstract = True
+
+
+class AbrasionResistance(models.Model):
+    abrasion_resistance_1 = models.IntegerField(verbose_name='Стойкость к истиранию 1 замер')
+    abrasion_resistance_2 = models.IntegerField(verbose_name='Стойкость к истиранию 2 замер')
+    abrasion_resistance_3 = models.IntegerField(verbose_name='Стойкость к истиранию 3 замер')
+    abrasion_resistance_sr = models.IntegerField(verbose_name='Стойкость к истиранию среднее')
+
+    class Meta:
+        verbose_name = 'Стойкость к истиранию'
+        verbose_name_plural = 'Стойкость к истиранию'
+        abstract = True
+
+
+class PencilHardness(models.Model):
+    pencil_hardness_1 = models.IntegerField(verbose_name='Прочность по карандашу 1 замер')
+    pencil_hardness_2 = models.IntegerField(verbose_name='Прочность по карандашу 2 замер')
+    pencil_hardness_3 = models.IntegerField(verbose_name='Прочность по карандашу 3 замер')
+    pencil_hardness_sr = models.IntegerField(verbose_name='Прочность по карандашу среднее')
+
+    class Meta:
+        verbose_name = 'Прочность по карандашу'
+        verbose_name_plural = 'Прочность по карандашу'
+        abstract = True
+
+
+class Eriksen(models.Model):
+    eriksen_1 = models.IntegerField(verbose_name='Прочность по Эриксену 1 замер')
+    eriksen_2 = models.IntegerField(verbose_name='Прочность по Эриксену 2 замер')
+    eriksen_3 = models.IntegerField(verbose_name='Прочность по Эриксену 3 замер')
+    eriksen_sr = models.IntegerField(verbose_name='Прочность по Эриксену среднее')
+
+    class Meta:
+        verbose_name = 'Прочность по Эриксену'
+        verbose_name_plural = 'Прочность по Эриксену'
+        abstract = True
+
+
+class ColorStandart(models.Model):
+    color_standart_1 = models.IntegerField(verbose_name='Стандарт цвета 1 замер')
+    color_standart_2 = models.IntegerField(verbose_name='Стандарт цвета 2 замер')
+    color_standart_3 = models.IntegerField(verbose_name='Стандарт цвета 3 замер')
+    color_standart_sr = models.IntegerField(verbose_name='Стандарт цвета среднее')
+
+    class Meta:
+        verbose_name = 'Стандарт цвета'
+        verbose_name_plural = 'Стандарт цвета'
+        abstract = True
+
+
+class LabTests(DryLayer, PrimerLayer, ReverseLayer, Brilliance, NotchedAdhesion,
+               ResistanceToShock, AbrasionResistance, PencilHardness, Eriksen, ColorStandart, models.Model):
     roll_number = models.ForeignKey(RollModified, on_delete=models.CASCADE, verbose_name='Номер рулона')
     ral = models.ForeignKey(Ral, on_delete=models.CASCADE, verbose_name='RAL')
     paint_man = models.ForeignKey(PaintMan, on_delete=models.CASCADE, verbose_name='Производитель краски')
-    dry_layer_thickness_1 = models.CharField(verbose_name='Толщина сухого слоя мкм 1 замер', max_length=20)
-    dry_layer_thickness_2 = models.CharField(verbose_name='Толщина сухого слоя мкм 2 замер', max_length=20)
-    dry_layer_thickness_3 = models.CharField(verbose_name='Толщина сухого слоя мкм 3 замер', max_length=20)
-    dry_layer_thickness_sr = models.CharField(verbose_name='Толщина сухого слоя мкм среднее', max_length=20)
-    primer_layer_thickness_1 = models.CharField(verbose_name='Толщина слоя грунта 1 замер', max_length=20)
-    primer_layer_thickness_2 = models.CharField(verbose_name='Толщина слоя грунта 2 замер', max_length=20)
-    primer_layer_thickness_3 = models.CharField(verbose_name='Толщина слоя грунта 3 замер', max_length=20)
-    primer_layer_thickness_sr = models.CharField(verbose_name='Толщина слоя грунта среднее', max_length=20)
-    reverse_layer_thickness_1 = models.CharField(verbose_name='Толщина обратного слоя мкм 1 замер', max_length=20)
-    reverse_layer_thickness_2 = models.CharField(verbose_name='Толщина обратного слоя мкм 2 замер', max_length=20)
-    reverse_layer_thickness_3 = models.CharField(verbose_name='Толщина обратного слоя мкм 3 замер', max_length=20)
-    reverse_layer_thickness_sr = models.CharField(verbose_name='Толщина обратного слоя мкм среднее', max_length=20)
-    brilliance_1 = models.CharField(verbose_name='Блеск % 1 замер', max_length=20)
-    brilliance_2 = models.CharField(verbose_name='Блеск % 2 замер', max_length=20)
-    brilliance_3 = models.CharField(verbose_name='Блеск % 3 замер', max_length=20)
-    brilliance_sr = models.CharField(verbose_name='Блеск % среднее', max_length=20)
-    notched_adhesion_1 = models.CharField(verbose_name='Адгезия с надрезом 1 замер', max_length=20)
-    notched_adhesion_2 = models.CharField(verbose_name='Адгезия с надрезом 2 замер', max_length=20)
-    notched_adhesion_3 = models.CharField(verbose_name='Адгезия с надрезом 3 замер', max_length=20)
-    notched_adhesion_sr = models.CharField(verbose_name='Адгезия с надрезом среднее', max_length=20)
-    resistance_to_shock_1 = models.CharField(verbose_name='Стойкость к обратному удару 1 замер', max_length=20)
-    resistance_to_shock_2 = models.CharField(verbose_name='Стойкость к обратному удару 2 замер', max_length=20)
-    resistance_to_shock_3 = models.CharField(verbose_name='Стойкость к обратному удару 3 замер', max_length=20)
-    resistance_to_shock_sr = models.CharField(verbose_name='Стойкость к обратному удару среднее', max_length=20)
-    abrasion_resistance_1 = models.CharField(verbose_name='Стойкость к истиранию 1 замер', max_length=20)
-    abrasion_resistance_2 = models.CharField(verbose_name='Стойкость к истиранию 2 замер', max_length=20)
-    abrasion_resistance_3 = models.CharField(verbose_name='Стойкость к истиранию 3 замер', max_length=20)
-    abrasion_resistance_sr = models.CharField(verbose_name='Стойкость к истиранию среднее', max_length=20)
     flexural_strength = models.CharField(verbose_name='Прочность при изгибе', max_length=20)
-    pencil_hardness_1 = models.CharField(verbose_name='Прочность по карандашу 1 замер', max_length=20)
-    pencil_hardness_2 = models.CharField(verbose_name='Прочность по карандашу 2 замер', max_length=20)
-    pencil_hardness_3 = models.CharField(verbose_name='Прочность по карандашу 3 замер', max_length=20)
-    pencil_hardness_sr = models.CharField(verbose_name='Прочность по карандашу среднее', max_length=20)
-    eriksen_1 = models.CharField(verbose_name='Прочность по Эриксену 1 замер', max_length=20)
-    eriksen_2 = models.CharField(verbose_name='Прочность по Эриксену 2 замер', max_length=20)
-    eriksen_3 = models.CharField(verbose_name='Прочность по Эриксену 3 замер', max_length=20)
-    eriksen_sr = models.CharField(verbose_name='Прочность по Эриксену среднее', max_length=20)
-    color_standart_1 = models.CharField(verbose_name='Стандарт цвета 1 замер', max_length=20)
-    color_standart_2 = models.CharField(verbose_name='Стандарт цвета 2 замер', max_length=20)
-    color_standart_3 = models.CharField(verbose_name='Стандарт цвета 3 замер', max_length=20)
-    color_standart_sr = models.CharField(verbose_name='Стандарт цвета среднее', max_length=20)
     total_thickness = models.CharField(verbose_name='Общая толщина', max_length=20)
     mass_finished = models.CharField(verbose_name='Масса готовой продукции', max_length=20)
     len_finished = models.CharField(verbose_name='Длина готовой продукции', max_length=20)
@@ -108,7 +189,8 @@ class LabTests(models.Model):
     comment = models.CharField(verbose_name='Примечания', max_length=20)
 
     def __str__(self):
-        return str(self.roll_number) + ' ' + str(self.ral) + ' ' + str(self.mass_finished) + ' ' + str(self.len_finished)
+        return (str(self.roll_number) + ' ' + str(self.ral) + ' '
+                + str(self.mass_finished) + ' ' + str(self.len_finished))
 
     class Meta:
         verbose_name = 'Лабораторные испытания'
